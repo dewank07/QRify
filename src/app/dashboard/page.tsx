84e8510e8ca60286f92f2d803ac4b882c19d80d9
dewank07 <dewankrastogi007@gmail.com>
2024-04-14 17:28:00 +0530

    feat: add landing page, Auth, Dashbaord

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Dashboard } from "@/layouts/dashboard";
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const page = () => {
+  return <Dashboard />;
+};
+
+export default page;
